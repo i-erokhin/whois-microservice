c67d64e6e75a8af406bd4aff72a6fedf895a94ac
Ilya Erokhin <i.erokhin@icloud.com>
2016-01-28 09:03:14 +0300

    Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   var content = exec('whois ' + request.url.substring(1), {silent:true}).output;
+  response.setHeader("Access-Control-Allow-Origin", "*");
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write(content);
   response.end();
-}).listen(8888);
+}).listen(8899);
