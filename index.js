28053ba17634a5d416430bd1a8f34353ce123611
Ilya Erokhin <i.erokhin@icloud.com>
2016-01-28 09:27:45 +0300

    regexp params check

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-#!/usr/bin/env node
+var PORT = 8899;
+var ALLOW_ORIGIN = true;
 
 require('shelljs/global');
 var http = require("http");
 
 http.createServer(function(request, response) {
-  var content = exec('whois ' + request.url.substring(1), {silent:true}).output;
-  response.setHeader("Access-Control-Allow-Origin", "*");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write(content);
+  if (ALLOW_ORIGIN) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+  }
+
+  var query = request.url.substring(1);
+
+  if (/^[\d\w\.-]+$/i.test(query)) {
+    var content = exec('whois ' + query, {silent:true}).output;
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(content);
+  } else {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write('Bad domain request: (' + query + ')');
+  }
   response.end();
-}).listen(8899);
+}).listen(PORT);
